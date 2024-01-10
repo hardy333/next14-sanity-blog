62c4d6026ff86e5ef4824ed00ea8a56ce8937736
Gela Samsonidze <samsonidze7@yahoo.com>
2024-01-11 03:02:37 +0400

    init commit

---
+++ app/_lib/interface.ts
@@ -0,0 +1,14 @@
+export interface simpleBlogCard {
+  title: string;
+  smallDescription: string;
+  slug: string;
+  titleImage: any;
+}
+
+export interface fullBlog {
+  currentSlug: string;
+  title: string;
+  content: any;
+  titleImage: any;
+  smallDescription: string;
+}
---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,50 @@
+import { fullBlog } from "@/app/_lib/interface";
+import { client, urlFor } from "@/app/_lib/sanity";
+import { PortableText } from "@portabletext/react";
+import Image from "next/image";
+
+
+export const revalidate = 0; // revalidate at most 30 seconds
+
+
+async function getFullBlogPost(slug: string) {
+  const query = `
+      *[_type == "blog" && slug.current == "${slug}"]{
+        title,
+        smallDescription,
+        "slug": slug.current,
+        titleImage,
+        content
+      }[0]
+    `;
+
+  const data = await client.fetch(query);
+
+  return data;
+}
+
+// shkjdhkjdh
+
+const FullBlogPost = async ({ params }: { params: { slug: string } }) => {
+  const data: fullBlog = await getFullBlogPost(params.slug);
+
+  console.log("single data", data);
+
+  return (
+    <main className="">
+      <div className="container" style={{ position: "relative" }}>
+        <h1>Hello</h1>
+        <Image src={urlFor(data.titleImage).url()} alt="image" width={1800} height={1000} />
+        <h2>{data.title}</h2>
+        <h4>{data.smallDescription}</h4>
+        <div className="full-blog-post-content">
+
+        <PortableText value={data.content} />
+        </div>
+        
+      </div>
+    </main>
+  );
+};
+
+export default FullBlogPost;
